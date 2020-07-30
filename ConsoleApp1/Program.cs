a3673649ac6d84b913db42ca2b8187d0650a9922
platonov.danik@bk.ru <platonov.danik@bk.ru>
2020-07-30 15:47:56 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+
+        static void Main()
+        {
+          Centure();
+        }
+        public static void Centure()
+        {
+            Console.WriteLine("Введите ваш год ");
+            int year = int.Parse (Console.ReadLine());
+            
+            if (year % 100 == 0)
+            {
+                
+                Console.Write
+                    ( year / 100  );
+                Console.WriteLine(" Век");
+            }
+            if (year % 100 != 0)
+            {
+                Console.Write((year / 100) + 1);
+                Console.WriteLine(" Век");
+            }
+        }
+    }    
+}
+
